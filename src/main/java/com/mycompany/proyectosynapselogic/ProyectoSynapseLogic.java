709b41f9ff6c7361464621d2459d0d61b959103a
snogu <snogu@MSI>
2026-06-02 18:50:54 -0400

    principio

---
+++ src/main/java/com/mycompany/proyectosynapselogic/ProyectoSynapseLogic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectosynapselogic;
+
+/**
+ *
+ * @author snogu
+ */
+public class ProyectoSynapseLogic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
