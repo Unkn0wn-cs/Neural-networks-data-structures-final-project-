2ef658186d5510b960b0b99980c1f1693a69cd5f
snogu <snogu@MSI>
2026-06-04 11:52:25 -0400

    Grafo listo y prueba de funcionamiento 2

--- src/main/java/com/mycompany/proyectosynapselogic/ProyectoSynapseLogic.java
+++ src/main/java/com/mycompany/proyectosynapselogic/ProyectoSynapseLogic.java
@@ -14,21 +14,6 @@ import Estructuras.RedNeuronal;
 public class ProyectoSynapseLogic {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        RedNeuronal miRed = new RedNeuronal(10);
-
-Neurona n1 = new Neurona("A", 1.0);
-Neurona n2 = new Neurona("B", 1.0);
-Neurona n3 = new Neurona("C", 1.0);
-
-miRed.agregarNeurona(n1);
-miRed.agregarNeurona(n2);
-miRed.agregarNeurona(n3);
-
-miRed.conectar("A", "B", 5.5, 1.2);
-miRed.conectar("A", "C", 3.0, 0.8);
-
-miRed.mostrarRed();
-    }
-    
+        System.out.println("Hello World!");  
+}
 }
